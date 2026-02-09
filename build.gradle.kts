115359528d6eea79842955eb0fbade434e641341
Phan Thị Hồng Thắm <thambocap191104@gmail.com>
2026-02-09 21:49:27 +0700

    FOOD-2 add base project

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.9.5"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
