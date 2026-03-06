1662501b31686b8eb92675fd05abfa22e17a762c
Phan Thị Hồng Thắm <thambocap191104@gmail.com>
2026-03-06 08:12:42 +0700

    add: home fragment with drawer layout

---
+++ app/src/main/java/com/example/tamangfood/presentation/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,34 @@
+package com.example.tamangfood.presentation.ui.favorite
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tamangfood.R
+import com.example.tamangfood.databinding.FragmentFavoriteBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/tamangfood/presentation/ui/home/FoodRecommendAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.tamangfood.presentation.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tamangfood.databinding.ItemFoodBestSellerCardBinding
+import com.example.tamangfood.databinding.ItemFoodRecommendCardBinding
+
+
+class FoodRecommendAdapter(
+    private val onItemClick: (FoodItem) -> Unit,
+) : ListAdapter<FoodItem, FoodRecommendAdapter.FoodRecommendViewHolder>(FoodRecommendDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodRecommendViewHolder {
+        val binding = ItemFoodRecommendCardBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return FoodRecommendViewHolder(binding, onItemClick)
+    }
+
+    override fun onBindViewHolder(holder: FoodRecommendViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class FoodRecommendViewHolder(
+        private val binding: ItemFoodRecommendCardBinding,
+        private val onItemClick: (FoodItem) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(food: FoodItem) {
+            binding.tvPrice.text = food.price
+            binding.ivFood.setImageResource(food.imageRes)
+        }
+    }
+
+    class FoodRecommendDiffCallback : DiffUtil.ItemCallback<FoodItem>() {
+        override fun areItemsTheSame(oldItem: FoodItem, newItem: FoodItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: FoodItem, newItem: FoodItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
